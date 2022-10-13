c1880d8adc569dde07b091561ddce78b574625f9
jose paterno <jpaterno85@gmail.com>
2022-10-12 17:07:47 -0700

    initial commit

---
+++ IsAnagram.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool isAnagram(string s,string t)
+{
+ string s1(s);
+ string s2(t);
+
+ sort(s1.begin(),s1.end());
+ sort(s2.begin(),s2.end());
+
+ return(s1.compare(s2)==0);
+
+}
+
+int main ()
+{
+ string s1("abc");
+ string s2("def");
+ if (isAnagram(s1,s2))
+  cout<<"it's an anagram"<<endl;
+ else
+  cout<<"it's not an anagram"<<endl;
+
+ return 0;
+}
